e69eaddd1de5c3ebeebc4261792180f6ddc64e68
João Gabriel <jgcamposneto@gmail.com>
2024-03-15 11:53:21 -0300

    Commit inicial

---
+++ src/main/java/br/com/fiap/postech/app/gestaoreserva/GestaoReservaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.app.gestaoreserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoReservaApplication.class, args);
+	}
+
+}
